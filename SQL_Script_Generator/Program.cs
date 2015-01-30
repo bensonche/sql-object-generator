1faa1940f860ba96372f18e23a418be9788d82eb
Benson Che <bche@resdat.com>
2015-01-30 14:47:14 -0900

    Refactor and add integrated security

--- SQL_Script_Generator/Program.cs
+++ SQL_Script_Generator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Dynamic;
@@ -7,23 +8,48 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace SQL_Script_Generator
 {
     internal class Program
     {
-        private const string username = "";
-        private const string password = "";
+        private static string username
+        {
+            get { return ConfigurationManager.AppSettings["username"]; }
+        }
 
-        private const string serverName = "";
-        private const string dbName = "";
+        private static string password
+        {
+            get { return ConfigurationManager.AppSettings["password"]; }
+        }
 
-        private const string outputDir = @"c:\temp\repeatable\triggers";
+        private static string serverName
+        {
+            get { return ConfigurationManager.AppSettings["serverName"]; }
+        }
+
+        private static string dbName
+        {
+            get { return ConfigurationManager.AppSettings["dbName"]; }
+        }
+
+        private static bool integrated
+        {
+            get { return ConfigurationManager.AppSettings["integrated"].ToLower() == "true"; }
+        }
+
+        private const string outputDir = @"c:\temp\repeatable\";
 
         private static string connectionString
         {
             get
             {
+                if(integrated)
+                    return string.Format(@"Server={0};Database={1};Trusted_Connection=True",
+                        serverName,
+                        dbName);
+
                 return string.Format(@"Server={0};Database={1};User Id={2};Password={3};",
                     serverName,
                     dbName,
@@ -38,6 +64,13 @@ namespace SQL_Script_Generator
 
             con.Open();
 
+            GetTriggers(outputDir, con);
+
+            con.Close();
+        }
+
+        private static void GetTriggers(string outputDir, SqlConnection con)
+        {
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = con;
             cmd.CommandType = CommandType.Text;
@@ -49,36 +82,36 @@ namespace SQL_Script_Generator
 
             var reader = cmd.ExecuteReader();
 
-            DirectoryInfo d = new DirectoryInfo(outputDir);
+            DirectoryInfo d = new DirectoryInfo(Path.Combine( outputDir, "triggers"));
 
             if (!d.Exists)
                 d.Create();
 
-            foreach(var f in d.GetFiles())
+            foreach (var f in d.GetFiles())
                 f.Delete();
 
             while (reader.Read())
             {
+                string name = reader["name"].ToString();
+                string definition = reader["definition"].ToString();
                 FileInfo f = new FileInfo(Path.Combine(d.FullName, string.Format("{0}.sql", reader["name"])));
 
                 using (Stream s = f.Create())
                 {
                     using (StreamWriter w = new StreamWriter(s))
                     {
-                        w.Write(@"IF  EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[{0}]'))
-DROP TRIGGER [dbo].[{0}]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-{1}
-GO", reader["name"], reader["definition"]);
+                        w.WriteLine(@"IF  EXISTS (SELECT * FROM sys.triggers WHERE object_id = OBJECT_ID(N'[dbo].[{0}]'))", name);
+                        w.WriteLine(@"DROP TRIGGER [dbo].[{0}]", name);
+                        w.WriteLine("GO");
+                        w.WriteLine("SET ANSI_NULLS ON");
+                        w.WriteLine("GO");
+                        w.WriteLine("SET QUOTED_IDENTIFIER ON");
+                        w.WriteLine("GO");
+                        w.WriteLine(definition);
+                        w.WriteLine("GO");
                     }
                 }
             }
-
-            con.Close();
         }
     }
 }
